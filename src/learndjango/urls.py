ee59d036227953557d0b19fd8c7369db3af1fce4
Ajay <majay1638@gmail.com>
2020-07-24 19:13:30 +0530

    added b=nvab

--- src/learndjango/urls.py
+++ src/learndjango/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages import views
-
+from products.views import product_detail_view
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('about', views.about_view, name='about'),
+    path('contact', views.contact_view, name='contact'),
+    path('product', product_detail_view, name='product'),
     path('admin/', admin.site.urls),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,8 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def home_view(*args, **kwargs):
-    return HttpResponse("Hello World")
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("Hello World")
+    return render(request, "home.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    # return HttpResponse("Hello World")
+    my_context = {
+        "text": "this is about me",
+        "list": ['item1', 'item2', 'item3']
+    }
+    return render(request, "about.html", my_context)
+
+
+def contact_view(request, *args, **kwargs):
+    # return HttpResponse("Hello World")
+    return render(request, "contact.html", {})
